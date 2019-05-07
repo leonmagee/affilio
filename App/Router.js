5930e258085fe8e67f68b6adcf4c7638f1002ebd
Leon Magee <leonmagee33@gmail.com>
2019-05-07 10:28:44 -0700

    react navigation added

--- App/Router.js
+++ App/Router.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import HomeImage from './HomeImage';
+import Promotions from './Promotions';
+import AddPromotion from './AddPromotion';
+import { colors } from './variables';
 
-class HomeScreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
+const TabNavigator = createBottomTabNavigator(
+  {
+    HomeImage,
+    Promotions,
+    AddPromotion,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'HomeImage') {
+          iconName = 'home';
+        } else if (routeName === 'Promotions') {
+          iconName = 'tag';
+        } else if (routeName === 'AddPromotion') {
+          iconName = 'tag-plus';
+        }
+
+        return <Icon name={iconName} size={35} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: colors.brandPrimary,
+      inactiveTintColor: '#DDD',
+      showLabel: false,
+      style: {
+        backgroundColor: '#222',
+        paddingTop: 15,
+      },
+    },
   }
-}
-
-class SettingsScreen extends Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings!</Text>
-      </View>
-    );
-  }
-}
-
-const TabNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen,
-});
+);
 
 export default createAppContainer(TabNavigator);
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /**
  * @format
  */
-
 import { AppRegistry, YellowBox } from 'react-native';
-import Homepage from './App/Homepage';
-// import Router from './App/Router';
+// import Homepage from './App/Homepage';
+import Router from './App/Router';
 // import AppMain from './AppMain';
 import { name as appName } from './app.json';
 
 YellowBox.ignoreWarnings(['Remote debugger', 'Require cycle:']);
 
-AppRegistry.registerComponent(appName, () => Homepage);
+AppRegistry.registerComponent(appName, () => Router);
